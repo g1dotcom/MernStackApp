4e8194655f3e48a5c4999f2db23846a4bd31e119
Gökhan Süle <thegkhnsl@gmail.com>
2023-02-25 15:25:44 +0300

    server tarafının oluşturulması

---
+++ server/Routers/userRouter.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { fullname, email, password, phoneNumber } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+      phoneNumber,
+    });
+    return res.status(201).json(createdUser);
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: "create user failed" });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Wrong Password" });
+
+    return res.status(200).json({ user, message: "Login Successful" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Login Failed" });
+  }
+});
+
+export default router;
